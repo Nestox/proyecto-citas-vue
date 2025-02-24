54f88c11be30b3629fa1d10c4c611432345d4a1f
Nestox <nestorgarr@gmail.com>
2025-02-24 21:16:25 +0100

    take 2
    - Recreated the project
    - Added a login and register interface

--- proyectCitas/src/views/HomeView.vue
+++ proyectCitas/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
+import LoginComp from '@/components/loginComp.vue';
 
 </script>
 
 <template>
   <main>
-    
+    <div class="mainContainer">
+      <LoginComp />
+    </div>
   </main>
 </template>
